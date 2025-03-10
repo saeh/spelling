64027720cb554bcc11101b3d13d2585780fa9914
saeh <atkins.tommy@gmail.com>
2025-03-10 10:47:38 +1000

    host locally

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def serve_audio(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
